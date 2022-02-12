8d4ed01bf35c9cc33a7a2bd81f91c49a868ec1e9
orshokety <orshokety@gmail.com>
2022-02-12 14:13:55 +0200

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import BlogList from "./BlogList";
+
+function Home() {
+  const [blogs, setBlogs] = useState(null);
+
+
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
+
+  return (
+    <div className="home">
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+function Navbar () {
+    return ( <nav className="navbar">
+        <h1>Ninja Blog</h1>
+        <div className="links">
+            <a href="/">Home</a>
+            <a href="/create" >New Blog</a>
+        </div>
+    </nav> );
+}
+ 
+export default Navbar;
